b4fd289ee903d63bdc111deadc15d15f8e8d1a75
Seydi Kamara <kamaraseydi7@gmail.com>
2026-09-22 01:21:53 +0200

    chore: initialiser le projet Spring Boot

---
+++ src/main/java/com/seydi/plateformereservationevenements/PlateformeReservationEvenementsApplication.java
@@ -0,0 +1,13 @@
+package com.seydi.plateformereservationevenements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformeReservationEvenementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlateformeReservationEvenementsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seydi/plateformereservationevenements/PlateformeReservationEvenementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seydi.plateformereservationevenements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateformeReservationEvenementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
