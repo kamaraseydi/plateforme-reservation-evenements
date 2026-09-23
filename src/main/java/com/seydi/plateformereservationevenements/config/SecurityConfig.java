8d95c13c610fdaeae745acff105db8bbfa4380b9
Seydi Kamara <kamaraseydi7@gmail.com>
2026-09-23 22:19:02 +0200

    feat: intégrer Supabase Auth avec Spring Security

---
+++ src/main/java/com/seydi/plateformereservationevenements/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.seydi.plateformereservationevenements.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(withDefaults())
+                );
+
+        return http.build();
+    }
+}
